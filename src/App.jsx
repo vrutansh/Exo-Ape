f29614cd7256076255e2145a2b51efa9dc092d93
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-25 13:13:53 +0530

    Added playreel section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Landing from './components/Landing'
 import Work from './components/Work'
+import Playreel from './components/Playreel'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Landing/>
       <Work/>
+      <Playreel/>
     </div>
     
   )
