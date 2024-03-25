c38138e621ca3cc1393c35bf91b130584d540632
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-25 13:19:47 +0530

    Completed.

--- src/components/Playreel.jsx
+++ src/components/Playreel.jsx
@@ -5,7 +5,7 @@ const Playreel = () => {
     <div className='w-full h-screen overflow-hidden relative bg-black'>
 
         <div className='md:w-96 w-40 aspect-video overflow-hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>
-            <video  autoPlay muted loop className='w-full h-full absolute top-1/2 left-1/2 scale-[2] -translate-x-1/2 -translate-y-1/2' src="https://download-video.akamaized.net/v3-1/playback/334619e0-f92f-4627-ba88-dfe521b4e062/9e976370?__token__=st=1711212140~exp=1711226540~acl=%2Fv3-1%2Fplayback%2F334619e0-f92f-4627-ba88-dfe521b4e062%2F9e976370%2A~hmac=d059bebe0d2c8acf14c94a1ac56bdf3d9d7d54376e4dc406bbf0854c92db9e2a&r=dXMtY2VudHJhbDE%3D"></video>
+            <video  autoPlay muted loop className='w-full h-full  absolute top-1/2 left-1/2 scale-[2] -translate-x-1/2 -translate-y-1/2' src="https://download-video.akamaized.net/v3-1/playback/334619e0-f92f-4627-ba88-dfe521b4e062/9e976370?__token__=st=1711352916~exp=1711367316~acl=%2Fv3-1%2Fplayback%2F334619e0-f92f-4627-ba88-dfe521b4e062%2F9e976370%2A~hmac=4deeb0769667c30dafb4bb9874acda6e6a5db7b8b9d5d70dea6e1b1e7e015481&r=dXMtY2VudHJhbDE%3D" type="video/mp4"></video>
         </div>
 
         <div className='overlay absolute w-full h-full  text-white flex flex-col justify-between py-20'>
