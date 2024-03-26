166f2fc017ead00df1db7f54a6aa1ff8b9f392c4
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-26 10:53:04 +0530

    playreel section done/..

--- src/components/Playreel.jsx
+++ src/components/Playreel.jsx
@@ -5,7 +5,7 @@ const Playreel = () => {
     <div className='w-full h-screen overflow-hidden relative bg-black'>
 
         <div className='md:w-96 w-40 aspect-video overflow-hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>
-            <video  autoPlay muted loop className='w-full h-full  absolute top-1/2 left-1/2 scale-[2] -translate-x-1/2 -translate-y-1/2' src="https://download-video.akamaized.net/v3-1/playback/334619e0-f92f-4627-ba88-dfe521b4e062/9e976370?__token__=st=1711352916~exp=1711367316~acl=%2Fv3-1%2Fplayback%2F334619e0-f92f-4627-ba88-dfe521b4e062%2F9e976370%2A~hmac=4deeb0769667c30dafb4bb9874acda6e6a5db7b8b9d5d70dea6e1b1e7e015481&r=dXMtY2VudHJhbDE%3D" type="video/mp4"></video>
+            <video  autoPlay muted loop className='w-full h-full  absolute top-1/2 left-1/2 scale-[2] -translate-x-1/2 -translate-y-1/2' src="https://player.vimeo.com/progressive_redirect/playback/914803778/rendition/1080p/file.mp4?loc=external&log_user=0&signature=5344c0e4fea63ca54bb433621ca0be7b9470b475583fa68b26de2b6e380a390a" type="video/mp4"></video>
         </div>
 
         <div className='overlay absolute w-full h-full  text-white flex flex-col justify-between py-20'>
