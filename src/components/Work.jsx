d5ca2594fd1d9167e584a3cd7aaeee853ca12c62
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-26 10:52:19 +0530

    edited

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,9 +4,11 @@ const Work = () => {
    const [elems, setelems] = useState([
         {heading: "PixelsFLake", Subheading: "Architectural Marketing Agency", video: "https://a.storyblok.com/f/133769/x/833a506717/pixel-flakes-hover.mp4", img:"https://a.storyblok.com/f/133769/2880x3588/5c50befb8e/pixel-flakes-hero.jpg/m/1300x1620/filters:format(webp):quality(70)"},
 
-        {heading: "Rino & Pelle", Subheading: "Effortless Chic Lifestyle", video: "https://download-video.akamaized.net/v3-1/playback/66710880-7060-4d65-965e-38a29da54c52/5ac47112?__token__=st=1711198507~exp=1711212907~acl=%2Fv3-1%2Fplayback%2F66710880-7060-4d65-965e-38a29da54c52%2F5ac47112%2A~hmac=1e5740714d019bdf4f20009c7da577299f54a1bf73e34a1fb5518eb4bb7e858f&r=dXMtY2VudHJhbDE%3D", img:"https://a.storyblok.com/f/133769/1606x2000/09a30057bd/aebele-interiors-hero.jpg/m/1300x1619/filters:format(webp):quality(70)"},
+        {heading: "Rino & Pelle", Subheading: "Effortless Chic Lifestyle", video: "https://a.storyblok.com/f/133769/x/88b4bf7989/news-rino-pelle.mp4",
+        
+        img:"https://a.storyblok.com/f/133769/1606x2000/09a30057bd/aebele-interiors-hero.jpg/m/1300x1619/filters:format(webp):quality(70)"},
 
-        {heading: "Abelle Interiors", Subheading: "Luxurious Desing Experience", video: "https://download-video.akamaized.net/v3-1/playback/57423169-59cb-4b8a-ad09-3bd812a12ee7/4ce94964?__token__=st=1711198533~exp=1711212933~acl=%2Fv3-1%2Fplayback%2F57423169-59cb-4b8a-ad09-3bd812a12ee7%2F4ce94964%2A~hmac=31fd4bd154189511bff65958c78f88619ed606131dc9f9189c9b10946f0a6b01&r=dXMtd2VzdDE%3D", img:"https://a.storyblok.com/f/133769/2500x3113/78edf86efd/rino-pelle-hero.jpg/m/1300x1619/filters:format(webp):quality(70)"},
+        {heading: "Abelle Interiors", Subheading: "Luxurious Desing Experience", video: "https://download-video.akamaized.net/v3-1/playback/d6b185a8-34d6-4821-b317-54ea0673a143/68caaacc?__token__=st=1711429132~exp=1711443532~acl=%2Fv3-1%2Fplayback%2Fd6b185a8-34d6-4821-b317-54ea0673a143%2F68caaacc%2A~hmac=8373da310f4256ae910d913bdd438651ab581aea317e2cd77d98a6167302a1b1&r=dXMtd2VzdDE%3D", img:"https://a.storyblok.com/f/133769/2500x3113/78edf86efd/rino-pelle-hero.jpg/m/1300x1619/filters:format(webp):quality(70)"},
 
         {heading: "Studio D", Subheading: "Urban and Landscape Design", video: "https://a.storyblok.com/f/133769/x/833a506717/pixel-flakes-hover.mp4", img:"https://a.storyblok.com/f/133769/2880x3588/5c50befb8e/pixel-flakes-hero.jpg/m/1300x1620/filters:format(webp):quality(70)"},
     ])
@@ -22,7 +24,7 @@ const Work = () => {
 
             <div className='elems md:flex md:flex-wrap md:gap-5 mt-10'>
                 {elems.map((item,index)=>{
-                  return  <div className='elem w-full md:w-[48%] mt-8 '>
+                  return  <div key={index} className='elem w-full md:w-[48%] mt-8 '>
                             <div className='video w-full h-[104vw] md:h-[50vw] relative overflow-hidden'>
                             <img className='w-full h-full object-cover hidden md:block' src={item.img} alt="" />
 
