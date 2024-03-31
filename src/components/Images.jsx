3832f5a6832b57d79d26b8ae17b7b87091bf2ed6
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-31 14:55:46 +0530

    last commit

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -24,15 +24,15 @@ const Images = () => {
       ease: Linear
     }, 'a')
     tl.to(second.current, {
-      x: "40%",
+      x: "-40%",
       ease: Linear
     }, 'a')
     tl.to(third.current, {
-      x: "10%",
+      x: "-40%",
       ease: Linear
     }, 'a')
     tl.to(fourth.current, {
-      x: "90%",
+      x: "40%",
       ease: Linear
     }, 'a')
 
@@ -42,16 +42,16 @@ const Images = () => {
   return (
     <div ref={parent} className='w-full md:h-[100vh] h-[70vh] bg-white flex items-center justify-center overflow-hidden'>
        <div className='w-[40%] md:w-[25%] md:h-[85%] h-1/2 relative'>
-         <div ref={first} className='w-20 h-[7rem] md:w-40 md:h-[15rem] -right-1/3 top-6  absolute'>
+         <div ref={first} className='w-20 h-[7rem] md:w-[14rem] md:h-[15rem] -right-1/3 top-6  absolute'>
            <img className='w-full h-full object-cover' src="https://a.storyblok.com/f/133769/348x494/21becfd449/home-news-3.jpg/m/550x781/filters:format(webp):quality(70)" alt="" />
          </div>
-         <div className='absolute md:w-[18rem] w-[8rem] aspect-video md:-left-2/3 -left-1/2 top-1/3 overflow-hidden'>
+         <div className='absolute md:w-[22rem] w-[8rem] aspect-video md:-left-2/3 -left-1/2 top-1/3 overflow-hidden'>
             <video ref={second} autoPlay muted loop className='w-full h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' src="https://a.storyblok.com/f/133769/x/9c433c7aac/home-news-diesel-be-a-follower.mp4"></video>
          </div>
-         <div ref={third} className='absolute md:w-[17rem] w-[9rem] aspect-video -left-[70%] -bottom-10 bg-red-400'>
+         <div ref={third} className='absolute md:w-[20rem] w-[9rem] aspect-video -left-[70%] -bottom-10 bg-red-400'>
            <img className='h-full w-full object-cover' src="https://a.storyblok.com/f/133769/758x508/8a1ff60d00/home-news-4.jpg/m/1200x804/filters:quality(90)" alt="" />
          </div>
-         <div className='absolute w-[7rem] aspect-[1.5/1] md:-right-[50%] md:w-[14rem] -right-[70%] -bottom-[33%] md:-bottom-[20%] '>
+         <div className='absolute w-[7rem] aspect-[1.5/1] md:-right-[50%] md:w-[20rem] -right-[70%] -bottom-[33%] md:-bottom-[20%] '>
            <video ref={fourth} autoPlay muted loop src="https://a.storyblok.com/f/133769/x/88b4bf7989/news-rino-pelle.mp4"></video>
          </div>
 
